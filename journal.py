7cbcbf4633700ce7b3d188336ac44cb9143b774a
123-dot-com <123-dot-com@github.com>
2026-01-31 11:41:26 +0530

    Updated journal.py - finished options, allowing adding emotions, removing emotions, printing emotions, and importing a pre-existing list

--- journal.py
+++ journal.py
@@ -7,17 +7,52 @@ import pandas as pd
 
 #Need to implement tkinter here in future
 def devPanel():
-    emotions = pd.readcsv(Emotions.csv, ignore_index=true)
-    while(true):
-        choice = input ("1. Input emotion \n2. Edit existing emotions \n3. View list \n4. Import list \n5. Exit options")
+    sort_dict = {'up':0,'middle':1,'down':2}
+    emotions = pd.read_csv("Emotions.csv", index_col=0)
+    while True:
+        choice = int(input ("1. Input emotion \n2. View list \n3. Edit existing emotions \n4. Import list \nAny. Exit options \n"))
         match choice:
             case 1:
+                emotions.loc[len(emotions)] = [input("Enter name of feeling: ").lower(),
+                                            input("Enter 'up' if good, 'middle' if okay, and 'down' if bad: ").lower()]
+                emotions.drop_duplicates(inplace=True)
+                emotions.to_csv("Emotions.csv", index=False)
+
+            case 2:
+                try:
+                    emotions.drop("Unnamed: 0", axis=1, inplace=True)
+                    emotions = emotions.iloc[emotions['emotion'].map(sort_dict).sort_values().index]
+
+                except:
+                    print()
+
+                finally:
+                    print(emotions,"\n")
+
+            case 3:
+                emotions.drop(int(input("Enter row number to delete: ")), inplace=True)
+                emotions.to_csv("Emotions.csv", index=False)
+
+            case 4:
+                print("To import a list, name the csv file as 'Emotions.csv' and place it in this directory (folder in which this program is located). \nIf it isn't working, check the name, which is case sensitive and needs to be exact.")
+
+            case _:
+                return
+
+
+
+
+
 
 
 
 
 
 print("Hey friend, welcome to this journal thingy~!")
-feeling = input("How are you doing today? (input 1 for dev panel)")
+feeling = input("How are you doing today? (input 1 for dev panel) \n")
 feeling = feeling.lower()
-devPanel()
+
+if feeling == "1":
+    devPanel()
+
+
