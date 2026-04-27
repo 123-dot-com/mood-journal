81cb80b1f6e68fb2aa3d63ff2a40925f07a4313e
123-dot-com <cuber.akshat@gmail.com>
2026-04-27 12:30:30 +0530

    Cleaned up journal.py

--- journal.py
+++ journal.py
@@ -14,12 +14,12 @@ causes = pd.DataFrame()
 def importFiles():
     try:
         emotions = pd.read_csv("Emotions.csv", index_col=0)
-        causes = pd.read_csv("Causes.csv",)
+        causes = pd.read_csv("Causes.csv", index_col=0)
 
     except:
         print ("The required csv files are either missing or empty, creating new ones")
         emotions = pd.DataFrame({"feeling":[], "emotion": []})
-        causes = pd.DataFrame({"feeling": [], "cause": [], "reflection":[], "time":[]})
+        causes = pd.DataFrame({"feeling": [], "emotion": [], "cause": [], "reflection":[], "date": [], "time": []})
 
     finally:
         emotions.to_csv("Emotions.csv")
@@ -30,7 +30,13 @@ def devPanel(): #Settings/developer panel to edit Emotions.csv file
     global emotions
     sort_dict = {'up':0,'middle':1,'down':2}
     while True:
-        choice = int(input ("1. Input emotion \n2. View list \n3. Edit existing emotions \n4. Import list \nAny. Exit options \n"))
+        choice = int(input ('''
+1. Input emotion 
+2. View list 
+3. Edit existing emotions 
+4. Import list 
+0. Exit options
+'''))
         match choice:
             case 1:
                 new_emotion = pd.DataFrame({"feeling": [input("Enter name of feeling: ").lower()],
@@ -54,26 +60,27 @@ def devPanel(): #Settings/developer panel to edit Emotions.csv file
                 emotions.to_csv("Emotions.csv")
 
             case 4:
-                print('''Names of csv files: 
-                    Emotions.csv - for the list of feelings and the respective emotion
-                    Causes.csv - for the list of feelings, their causes, the reflections, and the time at which it was felt
+                print('''
+Names of csv files: 
+Emotions.csv - for the list of feelings and the respective emotion
+Causes.csv - for the list of feelings, their causes, the reflections, and the time at which it was felt
 
-                    To import a list, name the csv file as required and place it in this directory: ''', os.getcwd(),
-                    '''\nIf it isn't working, check the name, which is case sensitive and needs to be exact.
-                    Also, for correct displaying of current list, make sure that your csv files are formatted only as follows (make sure to delete any other columns:
+To import a list, name the csv file as required and place it in this directory: ''', os.getcwd(),'''
+If it isn't working, check the name, which is case sensitive and needs to be exact.
+Also, for correct displaying of current list, make sure that your csv files are formatted only as follows (make sure to delete any other columns:
 
-                    Emotions.csv:
-                    feeling, emotion
+Emotions.csv:
+, feeling, emotion
 
-                    Causes.csv:
-                    feeling, cause, reflection, time
-                    ''')
+Causes.csv:
+, feeling, emotion, cause, reflection, date, time
+''')
             
             case _:
                 return
 
 
-def findCause(emotion): #To find the cause of the emotion, and reflection
+def findCause(feeling, emotion): #To find the cause of the emotion, and reflection
     global causes
     reflection = ""
     print("Please answer in one word")
@@ -81,15 +88,15 @@ def findCause(emotion): #To find the cause of the emotion, and reflection
         cause = input("That's great! What's making you feel that way? \n").lower()
 
     elif emotion == "middle":
-        cause = input("No problem, you don't always have to feel so good. What's making you feel so, though? \n").lower()
+        cause = input("No problem, you don't always have to feel so good. What's making you feel so? \n").lower()
 
     else:
-        cause = input("Oh no. What's making you feel so down today? \n").lower()
+        cause = input("What's making you feel so down today? \n").lower()
 
     if input("Do you want to reflect on this? (y/n): ").lower() == 'y':
         reflection = reflect()
 
-    causes.loc[len(causes)] = [emotion, cause, reflection, dt.datetime.now()]
+    causes.loc[len(causes)] = [feeling, emotion, cause, reflection, dt.date.today(), dt.datetime.now().time())]
     causes.sort_values(by=['time'], ascending=False)
 
 
@@ -103,26 +110,24 @@ def reflect(): #To let multi-line reflection
 
 #FINISH ANALYSE
 def analyse(feeling, emotion):
-    choice = input("1. Check past trends for current emotion \n2. Plot mood over certian time")
+    choice = input("1. Check counts for feelings \n2. Plot mood over certain time")
 
 
 
 importFiles()
 print("Hey friend, welcome to this journal thingy!")
-feeling = input("How are you doing today? (input 1 for dev panel) \n").lower()
+current_feeling = input("How are you doing today? (input 1 for dev panel) \n").lower()
 
-if feeling == "1":
+if current_feeling == "1":
     devPanel()
-
 
 else:
     try:
-        current_emotion = emotions.at[feeling, "emotion"]
-        findCause(emotion)
-
+        current_emotion = emotions.at[current_feeling, "emotion"]
+        findCause(current_feeling, current_emotion)
         choice = input("Would you like to analyse your previous emotions? (y/n): ").lower()
         if choice == "y":
-            analyse(feeling, emotion)
+            analyse(current_feeling, current_emotion)
 
     except KeyError:
         choice = input("Feeling not recognised, do you want to add in settings? (y/n): ").lower()
@@ -130,8 +135,8 @@ else:
             devPanel()
 
     finally:
-        emotions.to_csv("Emotions.csv", index=False)
-        causes.to_csv("Causes.csv", index=False)
+        emotions.to_csv("Emotions.csv")
+        causes.to_csv("Causes.csv")
         print("Thank you for updating your journal. Check back in whenever you need to log your mood again :)")
 
 
